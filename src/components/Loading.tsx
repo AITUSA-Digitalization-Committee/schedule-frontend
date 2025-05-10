0eff721523b1c2a4292e0a3fd67d1d0c4d784dfe
Yerassyl <108489760+yeunikey@users.noreply.github.com>
2025-05-11 02:29:54 +0500

    loader, animations

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -6,7 +6,7 @@ interface LoadingProps {
 function Loading({ className }: LoadingProps) {
     return (
         <div className={`${className} flex justify-center items-center`}>
-            <div className="w-8 h-8 border-primary border-4 rounded-full border-t-transparent animate-spin" />
+            <div className="w-8 h-8 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin" />
         </div>
     );
 }
--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -1,4 +1,7 @@
-import * as Dialog from '@radix-ui/react-dialog';
+import { useEffect, useState } from 'react';
+
+import Loading from './Loading';
+import { cn } from '@/util/utils';
 
 interface MapProps {
     open: boolean,
@@ -7,21 +10,44 @@ interface MapProps {
 }
 
 function MapContainer({ open, setOpen, location }: MapProps) {
+    const [loading, setLoading] = useState(true);
+
+    // Сброс загрузки при открытии окна
+    useEffect(() => {
+        if (open) setLoading(true);
+    }, [open, location]);
+
     return (
-        <Dialog.Root open={open} onOpenChange={setOpen}>
-            <Dialog.Portal>
-                <Dialog.Overlay className="fixed inset-0 bg-black/40" onClick={() => setOpen(false)} />
+        <div className='fixed z-50'>
+            <div className={cn("fixed inset-0 bg-black/40",
+                open ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0',
+                "transition-all duration-200"
+            )} onClick={() => setOpen(false)} />
 
-                <div className="fixed bottom-0 left-0 right-0 bg-white rounded-t-2xl shadow-lg h-96 overflow-hidden">
-                    <div className="absolute top-0 mt-1 flex w-full justify-center">
-                        <div className="w-12 h-1 bg-gray-300 rounded-full mx-auto" />
-                    </div>
-
-                    <iframe src={"https://map.yeunikey.dev?location=" + location} width={'100%'} height={'100%'}></iframe>
-
+            <div className={cn("fixed bottom-0 left-0 right-0 bg-white rounded-t-2xl shadow-lg h-96 overflow-hidden",
+                open ? 'translate-y-0' : 'translate-y-full',
+                "transition-all duration-300"
+            )}>
+                <div className="absolute top-0 mt-1 flex w-full justify-center z-10">
+                    <div className="w-12 h-1 bg-gray-300 rounded-full mx-auto" />
                 </div>
-            </Dialog.Portal>
-        </Dialog.Root>
+
+                {/* Лоадер */}
+                {loading && (
+                    <div className="absolute inset-0 z-10 flex items-center justify-center bg-white">
+                        <Loading />
+                    </div>
+                )}
+
+                <iframe
+                    src={"https://map.yeunikey.dev?location=" + location}
+                    width={'100%'}
+                    height={'100%'}
+                    onLoad={() => setLoading(false)}
+                    className="relative z-0"
+                ></iframe>
+            </div>
+        </div>
     );
 }
 
